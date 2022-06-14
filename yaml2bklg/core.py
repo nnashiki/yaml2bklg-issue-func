d441fd4ec2a13f38195956c13f202aeecd9a14d8
Niten Nashiki <niten.nashiki@lightz-inc.com>
2022-06-15 00:10:39 +0900

    update: parent

--- yaml2bklg/core.py
+++ yaml2bklg/core.py
@@ -8,6 +8,7 @@ class BacklogIssueAddReq:
     description: str
     issueTypeId: int
     priorityId: int = 3
+    parentIssueId: int = None
 
 
 def add_issues(parent_req: BacklogIssueAddReq, child_issues_req: list[BacklogIssueAddReq]):
